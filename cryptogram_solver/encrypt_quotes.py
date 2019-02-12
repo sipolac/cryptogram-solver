b2b75c637afc8a4f30a20a3d2a95233fffdc4a57
Christopher Sipola <chris.sipola@tellic.com>
2019-02-12 18:51:55 -0500

    Create saver and loader for solver and move argparsing location

---
+++ cryptogram_solver/encrypt_quotes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Authors: Chris Sipola
+Created: 2019-02-10
+
+Encrypt quotes file.
+"""
+import random
+
+from cryptogram_solver import solver
+from cryptogram_solver import utils
+
+
+PROJECT_DIR = utils.get_project_dir()
+quotes_path = PROJECT_DIR / 'references' / 'quotes.txt'
+quotes_encrypted_path = PROJECT_DIR / 'references' / 'quotes_encrypted.txt'
+
+
+def main():
+    random.seed(1234)
+    with open(quotes_path) as f:
+        quotes = f.read()
+    slv = solver.Solver(None, None, None)
+    quotes_encrypted = slv.encrypt(quotes).upper()
+    with open(quotes_encrypted_path, 'w') as f:
+        f.write(quotes_encrypted)
+
+
+if __name__ == '__main__':
+    main()
